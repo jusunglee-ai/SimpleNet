586ab7ac379a4a5a6cc47793467c1aa7a91d3458
jusunglee-ai <125032849+jusunglee-ai@users.noreply.github.com>
2024-09-28 09:43:42 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
-datapath=/data4/MVTec_ad
-datasets=('screw' 'pill' 'capsule' 'carpet' 'grid' 'tile' 'wood' 'zipper' 'cable' 'toothbrush' 'transistor' 'metal_nut' 'bottle' 'hazelnut' 'leather')
+#!/bin/bash
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate py38
+datapath="C:\Users\bjhan\Desktop\SimpleNet-main-original\datasets\MoP-MCAD"
+datasets=('screw')
 dataset_flags=($(for dataset in "${datasets[@]}"; do echo '-d '"${dataset}"; done))
 
-python3 main.py \
---gpu 4 \
+python main.py \
+--gpu 0 \
 --seed 0 \
 --log_group simplenet_mvtec \
 --log_project MVTecAD_Results \
@@ -16,7 +19,7 @@ net \
 --pretrain_embed_dimension 1536 \
 --target_embed_dimension 1536 \
 --patchsize 3 \
---meta_epochs 40 \
+--meta_epochs 5 \
 --embedding_size 256 \
 --gan_epochs 4 \
 --noise_std 0.015 \
